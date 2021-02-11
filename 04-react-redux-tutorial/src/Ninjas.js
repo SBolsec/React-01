4be360dc12c3dcf41bed5a4df019c07f45032e40
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-02-11 16:46:56 +0100

    conditionals

--- 04-react-redux-tutorial/src/Ninjas.js
+++ 04-react-redux-tutorial/src/Ninjas.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-const Ninjas = ({ninjas}) => {
-    const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-        );
-    });
+const Ninjas = ({ ninjas }) => {
+    const ninjaList = ninjas
+        .filter(ninja => {
+            return ninja.age > 20;
+        }).map(ninja => {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+            );
+        });
 
     return (
         <div className="ninja-list">
